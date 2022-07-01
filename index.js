cf50dfb959bddc326945bc4aafcb8e3f5700ddbe
Olivia <oliviatmanalastas@gmail.com>
2022-07-01 16:21:28 -0400

    Added Pictures

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 const classSelector = document.getElementById('class-select')
+const baseImgUrl = 'https://www.dndbeyond.com/attachments/thumbnails/0/'
+const portraitMap = {
+    'barbarian': '679/400/417/c3barbarianintro.png',
+    'bard': '684/400/406/c3bardintro.png',
+    'cleric': '687/380/437/c3clericintro.png',
+    'druid': '693/400/399/c3druidintro.png',
+    'fighter': '697/400/475/c3fighterintro.png',
+    'paladin': '701/400/473/c3paladinintro.png',
+    'monk': '700/400/490/c3monkintro.png',
+    'ranger': '707/400/444/c3rangerintro.png',
+    'rogue': '709/375/480/c3rogueintro.png',
+    'sorcerer': '712/400/517/c3sorcererintro.png',
+    'wizard': '717/400/484/c3wizardintro.png',
+    'warlock': '716/400/512/c3warlockintro.png'
+}
 
 let request = async () => {
     // initiate request to dnd server
@@ -12,6 +27,14 @@ let request = async () => {
         option.setAttribute('value' , char.name)
         option.innerText = char.name
         classSelector.append(option)
+    })
+
+    classSelector.addEventListener('change' , (e) => {
+        document.getElementById('selected-class').innterText = e.target.value
+        let img = document.createElement('img')
+        img.id = 'char-ming'
+        img.src = `${portraitMap}`
+
     })
 }
 
